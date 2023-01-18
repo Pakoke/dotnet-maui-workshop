24013a9f3cb6a02cb20724e48c3006d51ee10e23
javi ruiz de la torre <jrdlt1991@hotmail.com>
2023-01-18 23:43:18 +0100

    Add MonkeyView and MonkeyViewModel. Add httprequest

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,61 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class BaseViewModel
+namespace MonkeyFinder.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel()
+    {
+
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !isBusy;
+
 }
+//public class BaseViewModel : INotifyPropertyChanged
+//{
+//    bool isBusy;
+//    string title;
+//    public bool IsBusy
+//    {
+//        get { return isBusy; }
+//        set 
+//        { 
+//            if(isBusy == value)
+//            {
+//                return;
+//            }
+//            isBusy = value;
+//            OnPropertyChanged(nameof(IsBusy));
+//            OnPropertyChanged(nameof(IsNotBusy));
+//        }
+//    }
+
+//    public string Title
+//    {
+//        get { return title; }
+//        set
+//        {
+//            if (title == value)
+//                return;
+//            title = value;
+//            OnPropertyChanged(nameof(Title));
+//        }
+//    }
+
+//    public bool IsNotBusy => !IsBusy;
+
+//    public event PropertyChangedEventHandler PropertyChanged;
+
+//    public void OnPropertyChanged([CallerMemberName]string name = null)
+//    {
+//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+//    }
+//}
